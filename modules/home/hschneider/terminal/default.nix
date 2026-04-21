fbd28931fdab787d186b6aa2f73e38ac6db2256d
Holger Schneider <hschneider@Holgers-MBP.fritz.box>
2026-04-21 15:42:20 +0200

    set up zsh

--- modules/home/hschneider/terminal/default.nix
+++ modules/home/hschneider/terminal/default.nix
@@ -1,68 +1,37 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-
-  imports = [
-    ./starship/default.nix
-  ];
-
-  # Some user-level dotfiles
-  home.file = {
-    # Configure ssh
-#    ".ssh/config".text = ''
-#      Host *
-#        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-#    '';
-
-    # Configure git
-#    ".gitconfig".text = ''
-#      [user]
-#        name = swaphb
-#        email = s@swaphb.com
-#        signingkey = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJx0WMlfx+AwcROXFO+/all/WkLvBKpEkjwRY15tjSiB
-
-#      [gpg]
-#        format = ssh
-
-#      [gpg "ssh"]
-#        program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-
-#      [commit]
-#        gpgSign = true
-#    '';
-
-    # Configure 1password agent.toml
-#    ".config/1Password/ssh/agent.toml".text = ''
-      # Examples can be found here:
-      #  https://developer.1password.com/docs/ssh/agent/config
-
-#      [[ssh-keys]]
-#      item = "github-auth-key"
-#      vault = "Private"
-      
-#      [[ssh-keys]]
-#      item = "github-sign-key"
-#      vault = "Private"
-      
-#      [[ssh-keys]]
-#      item = "Github-swaphb"
-#      vault = "Private"
-      
-#      [[ssh-keys]]
-#      vault = "Employee"
-#    '';
-    
-    # Configure zsh
-    ".zshrc".text = ''
-      eval "$(starship init zsh)"
-      export PATH="''${KREW_ROOT:-/Users/${config.home.username}/.krew}/bin:$PATH"
-    '';
-
-    # Configure ghostty
-    ".config/ghostty/config".text = ''
-      background-opacity = 0.9
-      theme = "dracula"
-    '';
-
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    shellAliases = {
+      k = kubectl
+      ip = "ip --color=auto";
+    };
+                              '';
+    plugins = with pkgs; [
+      {
+        name = "zsh-syntax-highlighting";
+        src = fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.6.0";
+          sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
+        };
+        file = "zsh-syntax-highlighting.zsh";
+      }
+    ];
   };
+
+#  programs.fzf = {
+#    enable = true;
+#    enableZshIntegration = true;
+#  };
+
+  # Scripts
+#  home.file.".config/zsh/scripts".source = ./files/scripts;
+#  home.file.".config/zsh/scripts".recursive = true;
 }
