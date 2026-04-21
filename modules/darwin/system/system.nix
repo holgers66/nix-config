cf1c7b2b84b8b071033b335e324330e26c994414
Holger Schneider <holger.schneider@gmail.com>
2026-04-21 14:35:01 +0200

    fixing darwin

--- modules/darwin/system/system.nix
+++ modules/darwin/system/system.nix
@@ -19,11 +19,13 @@
     # Set up your keyboard preferences here
   };
 
+  system.primaryUser = 'hschneider'
+
   system.defaults.WindowManager.EnableStandardClickToShowDesktop = false; # Disable/Enable standard click to show desktop
   # You can add more Mac defaults here as well...
 
-  system.activationScripts.postUserActivation.text = ''
-    # Following line should allow us to avoid a logout/login cycle
-    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-  ''; # Activate settings after user activation
+#  system.activationScripts.postUserActivation.text = ''
+#    # Following line should allow us to avoid a logout/login cycle
+#    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+#  ''; # Activate settings after user activation
 }
