a86f414d10c92570bbbb8c7bb1b7adb7305fcb63
Holger Schneider <holger.schneider@gmail.com>
2026-04-22 16:01:04 +0200

    fixed dock

--- modules/darwin/system/appearance.nix
+++ modules/darwin/system/appearance.nix
@@ -9,9 +9,9 @@ in
     orientation = "bottom";
     persistent-apps = [
       "/Applications/Windows App.app"
-      "/Applications/Calendar.app"
-      "/Applications/System Settings.app"
-      "/Applications/Safari.app"
+      "/System/Applications/Calendar.app"
+      "/System/Applications/System Settings.app"
+      "/System/Applications/Safari.app"
       # Use the nixpkgs path to the app for apps installed via nix. This will automatically use the latest nix store path.
       "/${pkgs.slack}/Applications/Slack.app"
       "/${pkgs.vscode}/Applications/Visual Studio Code.app"
