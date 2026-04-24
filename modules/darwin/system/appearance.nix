b7dd9acab78bc4cde2fb6c9c56e4e23cabc65659
Holger Schneider <holger.schneider@gmail.com>
2026-04-24 12:54:59 +0200

    add helium and remove safari from dock

--- modules/darwin/system/appearance.nix
+++ modules/darwin/system/appearance.nix
@@ -8,10 +8,11 @@ in
     autohide = true;
     orientation = "bottom";
     persistent-apps = [
+      "/Applications/Helium.app"
       "/Applications/Windows App.app"
       "/System/Applications/Calendar.app"
       "/System/Applications/System Settings.app"
-      "/Applications/Safari.app"
+      # "/Applications/Safari.app"
       # Use the nixpkgs path to the app for apps installed via nix. This will automatically use the latest nix store path.
       "/${pkgs.slack}/Applications/Slack.app"
       "/${pkgs.vscode}/Applications/Visual Studio Code.app"
